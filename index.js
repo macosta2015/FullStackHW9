b82a1414b0e2d32c6efcbab24dec6765cec104a4
macosta2015 <macosta2015@my.fit.edu>
2022-05-23 20:45:22 -0400

    Using methods to create and read files

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-var fs = require('fs');
-
 const inquirer = require('inquirer');
 
-inquirer
-    .prompt([
-        {
-            type: "list", 
-            message: "Pick the framework your using:", 
-            name: "framework", 
-            choices: ["react","react-native","node"]
-        },
-    ])
-    .then(answers => {
-        answers.framework 
-        console.log(answers);
-    });
+var fs = require('fs');
+
+fs.readFile('readMe.txt', 'utf8');
+
+// var readMe = fs.readFileSync('readMe.txt', 'utf8');
+// var readMe = fs.readFile('readMe.txt', 'utf8');
+
+
+
+//We create a file in the following line!
+//We are using a second method for this
+fs.writeFileSync('writeMe.txt', readMe);
+
+console.log(readMe);
+
+// fd.writeFileSync('writeMe.txt',readme);
+
+// inquirer
+//     .prompt([
+//         {
+//             type: "list", 
+//             message: "Pick the framework your using:", 
+//             name: "framework", 
+//             choices: ["react","react-native","node"]
+//         },
+//     ])
+//     .then(answers => {
+//         answers.framework 
+//         console.log(answers);
+//     });
